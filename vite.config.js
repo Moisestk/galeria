cd1f3c3e675f0618405e99a881bb5dbe32efbcac
Moises Guerrero <moisestodoroki@gmail.com>
2025-01-27 17:32:39 -0400

    segundo comit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Aquí se indica que la carpeta de salida es 'dist'
+  }
 })
