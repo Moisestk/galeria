a3ed8a2b9154160692e1701676b5187d3cb842c6
Moises Guerrero <moisestodoroki@gmail.com>
2025-01-27 18:28:09 -0400

    cuarto comit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http//moisestk.github.io/galeria"
+  base: "https://moisestk.github.io/galeria"
 })
